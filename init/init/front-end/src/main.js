507b303786ffa003b5fb1aebfc2181de3debc532
theo <theogenix@gmail.com>
2023-03-28 16:22:32 +0200

    Styles with SAAS works

--- init/init/front-end/src/main.js
+++ init/init/front-end/src/main.js
@@ -7,8 +7,10 @@ import { WelcomeComponent } from "./app/components/welcome/welcome.component";
 import { NavbarComponent } from "./app/components/navbar/navbar.component";
 import { FooterComponent } from "./app/components/footer/footer.component";
 // TODO #import-css: use ES side-effect imports to import styles/style.css
-import "bootstrap/dist/css/bootstrap.css";
+
 import "./app/styles/style.scss";
+
+
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
 customElements.define("my-navbar", NavbarComponent);
